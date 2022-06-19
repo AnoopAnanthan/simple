909e5df457e06944957ae06aa72a92a258e71d4f
AnoopAnanthan <anoopananthan9@gmail.com>
2022-06-19 16:22:27 +0530

    new

---
+++ src/sample/task.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class task {
+	
+	public  static void method() {
+		System.out.println("anoop");
+		
+	}
+	public void method1() {
+		System.out.println("ajeeth");
+		
+		
+}
+	public static void main(String[] args) {
+		task.method();
+	}
+	}
